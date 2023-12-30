6d4747bdb4565bb14b0d0df87f1ba804afccb48d
Vital Vuillaume <vitalvuillaume@proton.me>
2023-12-30 11:04:40 +0100

    Add files via upload

---
+++ pixel-adventure-game-desktop.js
@@ -0,0 +1,29 @@
+/*if (window.location.protocol != "https:") {
+    window.location.protocol="https:";
+}*/
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = 'map.png';
+
+const playerImage = new Image();
+playerImage.src = 'player.png';
+
+image.onload = function() {
+    c.drawImage(image, -100, -200);
+    const scale = 10;
+    const newWidth = playerImage.width / scale;
+    const newHeight = playerImage.height / scale;
+    c.drawImage(
+        playerImage,
+        canvas.width / 2 - newWidth / 2,
+        canvas.height / 2 - newHeight / 2,
+        newWidth,
+        newHeight
+    );
+};
