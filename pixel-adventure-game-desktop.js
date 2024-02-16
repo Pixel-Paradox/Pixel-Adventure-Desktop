160126b96c8c4e1961d062ec40087be1bf12d336
Vital Vuillaume <vitalvuillaume@proton.me>
2024-02-16 22:14:02 +0100

    Add files via upload

--- pixel-adventure-game-desktop.js
+++ pixel-adventure-game-desktop.js
@@ -2,6 +2,10 @@
     window.location.protocol="https:";
 }*/
 
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+});
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -14,8 +18,43 @@ image.src = 'map.png';
 const playerImage = new Image();
 playerImage.src = 'player.png';
 
-image.onload = function() {
-    c.drawImage(image, -50, -200);
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position;
+        this.image = image;
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -50,
+        y: -200
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    background.draw();
     const scale = 10;
     const newWidth = playerImage.width / scale;
     const newHeight = playerImage.height / scale;
@@ -23,11 +62,47 @@ image.onload = function() {
         playerImage,
         canvas.width / 2 - newWidth / 2,
         canvas.height / 2 - newHeight / 2,
-        newWidth,
-        newHeight
+        newWidth, newHeight
     );
-};
 
-document.addEventListener("contextmenu", function (event) {
-    event.preventDefault();
+    if (keys.w.pressed) background.position.y += 3;
+    else if (keys.a.pressed) background.position.x += 3;
+    else if (keys.s.pressed) background.position.y -= 3;
+    else if (keys.d.pressed) background.position.x -= 3;
+}
+animate()
+
+window.addEventListener("keydown", function(e) {
+    switch (e.key) {
+        case "w":
+            keys.w.pressed = true;
+            break;
+        case "a":
+            keys.a.pressed = true;
+            break;
+        case "s":
+            keys.s.pressed = true;
+            break;
+        case "d":
+            keys.d.pressed = true;
+            break;        
+    }
+});
+
+window.addEventListener("keyup", function(e) {
+    switch (e.key) {
+        case "w":
+            keys.w.pressed = false;
+            break;
+        case "a":
+            keys.a.pressed = false;
+            break;
+        case "s":
+            keys.s.pressed = false;
+            break;
+        case "d":
+            keys.d.pressed = false;
+            break;        
+    }
+    console.log(keys);
 });
