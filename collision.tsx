d431c0365477ac402e965157c60c55d95731103b
Vital Vuillaume <vitalvuillaume@proton.me>
2024-03-04 21:15:20 +0100

    Delete collision.tsx

--- collision.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="git" name="collision" tilewidth="16" tileheight="16" tilecount="5625" columns="75">
- <image source="collision.png" width="1200" height="1200"/>
-</tileset>
